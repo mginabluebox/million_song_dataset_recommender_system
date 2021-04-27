5980d7c8f32651e578b9c060ce6a762c51bd497a
HengjialiXu <57606408+HengjialiXu@users.noreply.github.com>
2021-04-27 11:10:38 -0400

    modify preprocess

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -17,23 +17,18 @@ from pyspark.ml import Pipeline
 from pyspark.ml.feature import StringIndexer
 
 def main(spark, netID, fraction):
-	# try:
-	# 	print(spark.conf.get('spark.executor.memory'))
-	# 	print(spark.conf.get('spark.driver.memory'))
-	# except:
-	# 	pass
 
-	# specify path
-	path = f'hdfs:/user/{netID}/final_project/subsample/'
+    # specify path
+    path = f'hdfs:/user/{netID}/final_project/subsample/'
 
-	# read training file
+    # read training file
     save = str(round(fraction, 2)).split('.')[1]
     data_name = f'cf_train_{save}.parquet'
-	original = spark.read.parquet(path + data_name)
+    original = spark.read.parquet(path + data_name)
 
-	# index id
-	indexers = [StringIndexer(inputCol=column, outputCol=column+"_index",stringOrderType='alphabetAsc').fit(original) for column in ['user_id','track_id'] ]
-	pipeline = Pipeline(stages=indexers)
+    # index id
+    indexers = [StringIndexer(inputCol=column, outputCol=column+"_index",stringOrderType='alphabetAsc').fit(original) for column in ['user_id','track_id'] ]
+    pipeline = Pipeline(stages=indexers)
     indexed_df = pipeline.fit(original).transform(original)
     
     #change float index to int
@@ -43,22 +38,22 @@ def main(spark, netID, fraction):
 
 # Only enter this block if we're in main
 if __name__ == "__main__":
-	# change default spark context config
-	# conf = [('spark.executor.memory', '10g'), ('spark.driver.memory', '10g')]
-	# config = spark.sparkContext._conf.setAll(conf)
-	# spark.sparkContext.stop()
+    # change default spark context config
+    # conf = [('spark.executor.memory', '10g'), ('spark.driver.memory', '10g')]
+    # config = spark.sparkContext._conf.setAll(conf)
+    # spark.sparkContext.stop()
 
-	# Get user netID from the command line
-	parser = argparse.ArgumentParser(description='Index the training dataset.')
-	parser.add_argument('--netID',help='Enter your netID for constructing path to your HDFS.')
-	parser.add_argument('--fraction',type=float,help='Enter fraction of the dataset.')
-	args = parser.parse_args()
+    # Get user netID from the command line
+    parser = argparse.ArgumentParser(description='Index the training dataset.')
+    parser.add_argument('--netID',help='Enter your netID for constructing path to your HDFS.')
+    parser.add_argument('--fraction',type=float,help='Enter fraction of the dataset.')
+    args = parser.parse_args()
 
-	netID = args.netID
-	fraction = args.fraction
+    netID = args.netID
+    fraction = args.fraction
 
-	# Create the spark session object
-	spark = SparkSession.builder.appName('subsample').config('spark.blacklist.enabled',False).getOrCreate()
+    # Create the spark session object
+    spark = SparkSession.builder.appName('subsample').config('spark.blacklist.enabled',False).getOrCreate()
 
-	# Call our main routine
-	main(spark, netID, fraction)
+    # Call our main routine
+    main(spark, netID, fraction)
