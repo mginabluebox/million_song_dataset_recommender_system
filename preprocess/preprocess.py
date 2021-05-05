e40bc1c284722ef9dc929ff378ac942b3d1996be
Mars Huang <wh2103@hlog-2.hpc.nyu.edu>
2021-05-04 21:07:58 -0400

    [Add] preprocess for whole dataset

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -6,38 +6,63 @@ Usage:
 '''
 #Use getpass to obtain user netID
 import sys
-import argparse 
+import argparse
+
+from functools import reduce
 
 # And pyspark.sql to get the spark session
 from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql import DataFrame
 
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import StringIndexer
 
-def main(spark, netID, fraction):
+def union_all(*dfs):
+    return reduce(DataFrame.union, dfs)
 
-    # specify path
-    path = f'hdfs:/user/{netID}/final_project/subsample/'
-
-    # read training file
-    save = str(round(fraction, 2)).split('.')[1]
-    data_name = f'cf_train_{save}.parquet'
-    original = spark.read.parquet(path + data_name)
+def preprocess_data(data_paths, data_out_paths):
+    dfs = [spark.read.parquet(data_path) for data_path in data_paths]
+    original = union_all(*dfs) if len(dfs) > 1 else dfs[0]
 
     # index id
-    indexers = [StringIndexer(inputCol=column, outputCol=column+"_index",stringOrderType='alphabetAsc').fit(original) for column in ['user_id','track_id'] ]
+    indexers = [StringIndexer(inputCol=column, outputCol=column+"_index",
+                stringOrderType='alphabetAsc')
+                for column in ['user_id','track_id'] ]
     pipeline = Pipeline(stages=indexers)
-    indexed_df = pipeline.fit(original).transform(original)
-    
-    #change float index to int
-    indexed_df = indexed_df.selectExpr("cast(user_id_index as int) user_id_index", "cast(count as int) count","cast(track_id_index as int) track_id_index")
-    data_out_name = f'cf_train_{save}_indexed.parquet'
-    indexed_df.write.parquet(path + data_out_name)
-    
-    #show schema to check
-    indexed_df.printSchema()
+
+    for i in range(len(dfs)):
+        indexed_df = pipeline.fit(original).transform(dfs[i])
+
+        #change float index to int
+        indexed_df = indexed_df.selectExpr("cast(user_id_index as int) user_id_index",
+                                           "cast(count as int) count",
+                                           "cast(track_id_index as int) track_id_index")
+
+        indexed_df.write.parquet(data_out_paths[i])
+
+        #show schema to check
+        indexed_df.printSchema()
+
+def main(spark, netID, fraction):
+    out_path = f'hdfs:/user/{netID}/final_project/subsample/'
+    if fraction:
+        # specify path
+        path = f'hdfs:/user/{netID}/final_project/subsample/'
+        # read training file
+        save = str(round(fraction, 2)).split('.')[1]
+        data_name = f'cf_train_{save}.parquet'
+        data_out_name = f'cf_train_{save}_indexed.parquet'
+        preprocess_data([path + data_name], [out_path + data_out_name])
+    else:
+        path = f'hdfs:/user/bm106/pub/MSD/'
+        data_types = ['train', 'validation', 'test']
+        data_names = [f'cf_{data_type}.parquet' for data_type in data_types]
+        data_out_names = [f'cf_{data_type}_indexed.parquet' for data_type in data_types]
+        data_paths = [path + data_name for data_name in data_names]
+        data_out_paths = [out_path + data_out_name for data_out_name in data_out_names]
+        preprocess_data(data_paths, data_out_paths)
 
 # Only enter this block if we're in main
 if __name__ == "__main__":
@@ -49,14 +74,14 @@ if __name__ == "__main__":
     # Get user netID from the command line
     parser = argparse.ArgumentParser(description='Index the training dataset.')
     parser.add_argument('--netID',help='Enter your netID for constructing path to your HDFS.')
-    parser.add_argument('--fraction',type=float,help='Enter fraction of the dataset.')
+    parser.add_argument('--fraction',type=float,default=None,help='Enter fraction of the dataset.')
     args = parser.parse_args()
 
     netID = args.netID
     fraction = args.fraction
 
     # Create the spark session object
-    spark = SparkSession.builder.appName('subsample').config('spark.blacklist.enabled',False).getOrCreate()
+    spark = SparkSession.builder.appName('preprocess').config('spark.blacklist.enabled',False).getOrCreate()
 
     # Call our main routine
     main(spark, netID, fraction)
